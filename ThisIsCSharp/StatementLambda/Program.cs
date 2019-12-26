8450fda8bc06ab54fdcb09994d0cc2aa5a87c1d4
gangside <x_hojun@naver.com>
2019-12-26 23:56:57 +0900

    Add Programs [SimpleLambda], [StatementLambda], [FuncLambda], [ActionLambda]

---
+++ ThisIsCSharp/StatementLambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StatementLambda
+{
+    class Program
+    {
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            string[] arr = { "치킨", "피자", "족발" };
+
+            Concatenate concat = (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr){
+                    result += s;
+                }
+                return result;
+            };
+
+            Console.WriteLine(concat(arr));
+        }
+    }
+}
