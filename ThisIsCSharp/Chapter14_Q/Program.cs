5ba5fe156e8d3a13bd0c60498c9f3f5b17c9f575
gangside <x_hojun@naver.com>
2019-12-29 00:12:02 +0900

    Solve [Chapter14_Q1Q2]

---
+++ ThisIsCSharp/Chapter14_Q/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter14_Q1Q2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+            foreach (var item in array)
+            {
+                Action action = () => Console.WriteLine(item * item);
+
+                action.Invoke();
+            }
+        }
+    }
+}
