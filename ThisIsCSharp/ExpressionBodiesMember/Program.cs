d0b2c9182b4989f54bef183116c805f952be55e0
gangside <x_hojun@naver.com>
2019-12-28 23:52:57 +0900

    Add Programs [ExpressionTreeViaLambda] and [ExpressionBodiesMember]

---
+++ ThisIsCSharp/ExpressionBodiesMember/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpressionBodiesMember
+{
+    class FriendList
+    {
+        List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);
+        public void Remove(string name) => list.Remove(name);
+        public void PrintAll()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public FriendList() => Console.WriteLine("FriendList()");
+        ~FriendList() => Console.WriteLine("~FriendList()");
+
+        //public int Capacity => list.Capacity; //읽기 전용
+        public int Capacity // 속성
+        {
+            get => list.Capacity;
+            set => list.Capacity = value;
+        }
+
+        //public string this[int index] => list[index]; //읽기 전용
+        public string this[int index] {
+            get => list[index];
+            set => list[index] = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FriendList obj = new FriendList();
+            obj.Add("철수");
+            obj.Add("영희");
+            obj.Add("길동");
+            obj.Remove("철수");
+            obj.PrintAll();
+
+            Console.WriteLine($"{obj.Capacity}");
+            obj.Capacity = 10;
+            Console.WriteLine($"{obj.Capacity}");
+
+            Console.WriteLine($"{obj[0]}");
+            obj[0] = "둘리";
+            obj.PrintAll();
+        }
+    }
+}
---
+++ ThisIsCSharp/ExpressionTreeViaLambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeViaLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> expression = (a, b) => 1 * 2 + (a - b);
+
+            Func<int, int, int> func = expression.Compile();
+
+            //x = 10, y = 8;
+            Console.WriteLine($"1*2+({10}-{8}) = {func(10, 8)}");
+        }
+    }
+}
