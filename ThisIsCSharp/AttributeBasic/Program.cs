de9e9ffb0c4b9f392ce94253403cb2393870bf30
gangside <x_hojun@naver.com>
2020-01-11 23:08:57 +0900

    Add Program [AttributeBasic]

---
+++ ThisIsCSharp/AttributeBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AttributeBasic {
+    class MyClass {
+
+        [Obsolete("OldMethod는 폐기됐습니다. NewMethod를 사용하세요")]
+        public void OldMethod() {
+            Console.WriteLine("i'm old method");
+        }
+
+        public void NewMethod() {
+            Console.WriteLine("i'm new method");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            MyClass obj = new MyClass();
+
+            obj.OldMethod();
+            obj.NewMethod();
+        }
+    }
+}
