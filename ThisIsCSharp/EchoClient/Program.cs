77f2ccf6e6f52a490d5040a710069af60aff18b6
unknown <x_hojun@naver.com>
2020-02-05 10:10:31 +0900

    Add Program [EchoServer] and [EchoClient]

---
+++ ThisIsCSharp/EchoClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            if(args.Length < 4) {
+                Console.WriteLine("사용법: {0} <Bind IP> <Server IP> <Message>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            string bindIp = args[0];
+            int bindPort = Convert.ToInt32(args[1]);
+            string serverIp = args[2];
+            const int serverPort = 5425;
+            string message = args[3];
+
+            try {
+                IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                Console.WriteLine("클라이언트: {0}, 서버: {1}", clientAddress.ToString(), serverAddress.ToString());
+
+                //클라이언트는 서버와 통신된 어떤 하나의 공간으로 이해해도 될것같은데
+                TcpClient client = new TcpClient(clientAddress);
+                client.Connect(serverAddress);
+
+                byte[] data = System.Text.Encoding.Default.GetBytes(message);
+
+                //클라이언트의 흐름(전기같은)에 data를 흘려보낸다. 현재 클라이언트는 서버와 연결된 상태니까,
+                //서버로 자연스래 흘러가고 서버의 포트가 열려있고 전기를 받을 준비가 되어있다면 받아가겠지.
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("송신: {0}", message);
+
+                //이제 서버에서 수신도 받아봐야지
+                data = new byte[256];
+                string responseData = "";
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.Default.GetString(data, 0, bytes);
+                Console.WriteLine("수신: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("에코 클라이언트 종료");
+        }
+    }
+}
---
+++ ThisIsCSharp/EchoServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            if (args.Length < 1) {
+                //Process
+                Console.WriteLine("사용법 : {0} <BindIP>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            string bindIp = args[0];
+            const int bindPort = 5425;
+            TcpListener server = null;
+
+            try {
+                //IPEndPoint는 IP통신에 필요한 IP주소의 출입구를 나타내는 클래스입니다. (IP, Port에 대한 정보가 들어가있어요)
+                IPEndPoint localAdress = new IPEndPoint(IPAddress.Parse(bindIp), bindPort);
+
+                server = new TcpListener(localAdress);
+                server.Start(); //IP와 포트를 입력하고나서 서버를 만들고 실행하게 된다.
+                Console.WriteLine("에코 서버가 시작됐습니다");
+
+                while (true) {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("클라이언트 접속 : {0}", ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
+
+                    NetworkStream stream = client.GetStream();
+
+                    int length;
+                    string data = null;
+                    byte[] bytes = new byte[256];
+
+                    //클라이언트의 스트림에서 바이트형식으로 문자열을 받아 인코딩한 후, 다시 바이트로 변환하여 클라이언트에게 송신한다.
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) {
+                        data = Encoding.Default.GetString(bytes, 0, length);
+
+                        Console.WriteLine(String.Format("수신: {0}", data));
+       
+                        byte[] msg = Encoding.Default.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("송신: {0}", data);
+                    }
+
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (SocketException e) {
+                Console.WriteLine(e.Message);               
+            }
+            finally {
+                server.Stop();
+            }
+
+            Console.WriteLine("에코 서버 종료");
+        }
+    }
+}
